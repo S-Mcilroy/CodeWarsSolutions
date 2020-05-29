94c310bed8ce193a7ee400bb84d5bae2c28d2b54
Steven Mcilroy <stevenmcilroy@msn.com>
2020-05-29 10:55:04 +0100

    Solution Pete, The Baker

---
+++ PeteTheBaker.js
@@ -0,0 +1,9 @@
+// 5 kyu on CodeWars
+
+//Solution:
+
+function cakes(recipe, available) {
+  return Object.keys(recipe).reduce(function(val, ingredient) {
+    return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+  }, Infinity)
+}
